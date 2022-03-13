421d45d19b4779a9a81a20eb8f13e79afc65d52d
jyh <seedjyh@gmail.com>
2022-03-13 12:01:05 +0800

    dijkstra算法改成去重复边的形式，并提供了用模板参数选择边权类型的功能。

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -32,9 +32,21 @@ public:
 
     // 添加一个pair->value映射。不考虑重复pair的情况。
     // u < n, v < n, weights >= 0
-    void add(int u, int v, const WEIGHT &w) {
+    void add(int u, int v, const WEIGHT w) {
         this->pair2index[make_pair(u, v)] = this->weights.size();
         this->weights.push_back(w);
+    }
+
+    // 更新一个pair->value映射。如果pair重复，则保存最小的那个权。
+    // u < n, v < n, weights >= 0
+    void update(int u, int v, const WEIGHT w) {
+        auto ite = pair2index.find(make_pair(u, v));
+        if (ite == pair2index.end()) {
+            this->pair2index[make_pair(u, v)] = this->weights.size();
+            this->weights.push_back(w);
+        } else {
+            this->weights[ite->second] = min(this->weights[ite->second], w);
+        }
     }
 
     // 查询从u到v的边下标，如果没有则返回-1。
@@ -58,25 +70,29 @@ public:
 // 2 - addEdge 所有边
 // 3 - calc() 执行dijkstra算法。
 // 4 - getDistance / getPrev
+template<typename WEIGHT>
 class Dijkstra {
     int nodeCount; // 节点数
     // 邻接表
     vector<vector<int>> adjacentNodes; // [i]和节点i邻接的节点的下标。
-    vector<int> distance; // [i]从源到节点i的最短路长度。
+    vector<WEIGHT> distance; // [i]从源到节点i的最短路长度。
     vector<int> prev; // [i]从源到节点i的最短路径上，节点i的前一个节点的下标。-1表示没有或不知道前一个节点。
     vector<bool> visited; // [i]是否已求出从源到节点i的最短路。也可视为“是否在集合S中”。
     // 边
-    PairMap<int> edges;
+    PairMap<WEIGHT> edges;
+    // 无穷大
+    const WEIGHT infinite;
 public:
     // n是节点的数量。
-    Dijkstra(int n) :
+    Dijkstra(int n, WEIGHT infinite = makeInfinite()) :
             nodeCount(n),
-            adjacentNodes(n) {};
+            adjacentNodes(n),
+            infinite(infinite) {};
 
     // 添加一条边，0 <= u、v < nodeCount
-    void addEdge(int u, int v, int weight) {
+    void addEdge(int u, int v, WEIGHT weight) {
         adjacentNodes[u].push_back(v);
-        edges.add(u, v, weight);
+        edges.update(u, v, weight); // 如果有重复边，选择权重最小的。
     }
 
     // 查询下标为node的节点的邻接节点
@@ -91,15 +107,15 @@ public:
         public:
             int from; // S中的节点
             int to; // S外的节点
-            int weight;
+            WEIGHT weight;
 
             bool operator<(const EdgeInQueue &other) const {
                 return this->weight > other.weight;
             }
         };
-        priority_queue <EdgeInQueue> edgeQueue; // pair<边权, 未进S的节点下标>
+        priority_queue<EdgeInQueue> edgeQueue; // pair<边权, 未进S的节点下标>
         // S设成空集
-        distance.resize(nodeCount, INT_MAX);
+        distance.resize(nodeCount, infinite);
         prev.resize(nodeCount, -1);
         visited.resize(nodeCount, false);
         // 开始计算！
@@ -134,29 +150,55 @@ public:
     }
 
     // 获取各节点单源最短路的长度。
-    const vector<int> &getDistance() const {
+    // makeInfinite()表示不可达。
+    const vector<WEIGHT> &getDistance() const {
         return this->distance;
     }
 
     // 获取各节点单源最短路的上一个节点。
+    // -1表示没有上一个节点（可能是不可达，也可能是源节点）。
     const vector<int> &getPrev() const {
         return this->prev;
+    }
+
+    // 生成WEIGHT类型的最大值。也可用于判断是否不可达。
+    static WEIGHT makeInfinite() {
+        return ~((WEIGHT) 1 << (sizeof(WEIGHT) * 8 - 1));
     }
 };
 
 int main() {
-    Dijkstra d(4);
+    Dijkstra<int> d(5);
     d.addEdge(0, 2, 1);
     d.addEdge(0, 1, 7);
     d.addEdge(2, 3, 9);
     d.addEdge(2, 1, 2);
     d.addEdge(1, 3, 3);
+    d.addEdge(4, 0, 1);
     d.calc(0);
     for (auto dis : d.getDistance()) {
-        cout << "distance:" << dis << endl;
+        if (dis == d.makeInfinite()) {
+            cout << "distance: infinite" << endl;
+        } else {
+            cout << "distance:" << dis << endl;
+        }
     }
+//    distance:0
+//    distance:3
+//    distance:1
+//    distance:6
+//    distance: infinite
     for (auto prev : d.getPrev()) {
-        cout << "prev:" << prev << endl;
+        if (prev < 0) {
+            cout << "prev: none" << endl;
+        } else {
+            cout << "prev:" << prev << endl;
+        }
     }
+//    prev: none
+//    prev:2
+//    prev:0
+//    prev:1
+//    prev: none
     return 0;
 }
