d7c62260b3424936d8e7269a115aa114892fc94c
jyh <jyh@eastcom.com>
2022-11-28 10:03:40 +0800

    更新并查集模板错误。

--- union_find_set.cpp
+++ union_find_set.cpp
@@ -15,6 +15,7 @@ class UnionFindSet {
     vector<int> roots; // 每个节点的所属集合
     vector<int> sizes; // 每个集合的当前数量
 public:
+    // 创建并查集，这里 n 是节点数，节点编号 0 ~ n-1
     explicit UnionFindSet(int n): roots(n), sizes(n, 1) {
         iota(roots.begin(), roots.end(), 0);
     }
@@ -26,14 +27,23 @@ public:
             return roots[a];
         }
     }
+    // FindRootSize 返回 a 所在集合的大小（a不一定是根）
     int FindRootSize(int a) {
         return sizes[FindRoot(a)];
     }
+    // FindRootSize 返回 a 所在集合的根
     void Union(int a, int b) {
-        if (FindRootSize(a) > FindRootSize(b)) {
-            roots[FindRoot(b)] = FindRoot(a);
+        int bRoot = FindRoot(b);
+        int aRoot = FindRoot(a);
+        if (aRoot == bRoot) {
+            return;
+        }
+        if (FindRootSize(aRoot) > FindRootSize(bRoot)) {
+            sizes[aRoot] += sizes[bRoot];
+            roots[bRoot] = aRoot;
         } else {
-            roots[FindRoot(a)] = FindRoot(b);
+            sizes[bRoot] += sizes[aRoot];
+            roots[aRoot] = bRoot;
         }
     }
 };
@@ -44,8 +54,9 @@ int main() {
     ufs.Union(2, 5);
     ufs.Union(3, 6);
     ufs.Union(2, 3);
+    ufs.Union(8, 9);
     for (int i = 0; i < n; i++) {
-        cout << i << "->" << ufs.FindRoot(i) << endl;
+        cout << "i=" << i << ", root=" << ufs.FindRoot(i) << ", size=" << ufs.FindRootSize(i) << endl;
     }
     return 0;
 }
