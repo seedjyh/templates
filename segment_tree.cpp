059e3809fff76d6202d7b628bada55ab0fd2b1a0
jyh <seedjyh@gmail.com>
2024-01-14 11:20:25 +0800

    并查集支持long long

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,11 +1,13 @@
 // 线段树（添加key-value；求key区间最大value）
 // 原题：https://leetcode.cn/contest/weekly-contest-310/problems/longest-increasing-subsequence-ii/
 
+typedef long long LL;
+
 class RangeTree {
 public:
     int begin;
     int end;
-    int maxVal;
+    LL maxVal;
     RangeTree *left, *right;
     RangeTree(int b, int e): begin(b), end(e), maxVal(0), left(nullptr), right(nullptr){};
     // assurePos 确保pos节点存在。
@@ -13,7 +15,7 @@ public:
         if (begin == end) {
             return;
         }
-        int mid = (begin + end) / 2;
+        int mid = calcMid(begin, end);
         // left: [begin, mid]
         // right: [mid + 1, end]
         if (this->left == nullptr) {
@@ -29,24 +31,28 @@ public:
         }
     }
     // 仅修改pos节点的值，以及父节点、祖先节点等。
-    void setMaxVal(int pos, int val) {
+    void setMaxVal(int pos, LL val) {
+        this->maxVal = max(this->maxVal, val);
         if (begin == end) {
             this->maxVal = max(this->maxVal, val);
             return;
         }
-        int mid = (begin + end) / 2;
+        int mid = calcMid(begin, end);
         // left: [begin, mid]
         // right: [mid + 1, end]
+        if (this->left == nullptr) {
+            this->left = new RangeTree(begin, mid);
+            this->right = new RangeTree(mid + 1, end);
+        }
         if (pos <= mid) {
             this->left->setMaxVal(pos, val);
         } else {
             this->right->setMaxVal(pos, val);
         }
-        this->maxVal = max(this->left->maxVal, this->right->maxVal);
         return;
     }
     // findMaxVal 查找某区间里的最大值
-    int findMaxVal(int from, int to) const {
+    LL findMaxVal(int from, int to) const {
         if (this->begin > to || this->end < from) {
             return 0;
         }
@@ -56,7 +62,7 @@ public:
         if (from <= this->begin && this->end <= to) {
             return maxVal;
         }
-        int mid = (begin + end) / 2;
+        int mid = calcMid(begin, end);
         if (from > mid) {
             return this->right->findMaxVal(from, to);
         } else if (to <= mid) {
@@ -64,5 +70,9 @@ public:
         } else {
             return max(this->left->findMaxVal(from, mid), this->right->findMaxVal(mid + 1, to));
         }
+    }
+    int calcMid(int lo, int hi) const {
+        if (lo + hi >= 0) return (lo + hi) / 2;
+        else return (lo + hi - 1) / 2;
     }
 };
